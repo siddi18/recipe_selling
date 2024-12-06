f5aeafc415a2762acb4ed01b2fa0dfcad05abb4c
siddi <crazythoughts1729@gmail.com>
2024-12-06 18:00:28 +0530

    4th commit

--- app/api/recipes/route.js
+++ app/api/recipes/route.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { getUserMeals } from "../../../lib/meals";
 
 export async function GET(req) {
--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import bcrypt from 'bcryptjs';
 import { createUserWithAccount, getUserByEmail } from "../../../utils/user"; 
 
